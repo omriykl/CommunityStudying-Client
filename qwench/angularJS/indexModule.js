fb3feac004d498c8677bbde734059f05fee6db24
omri <omri@tokenads.com>
2016-12-12 00:10:41 +0200

    using angularJS-route for Routing pages

--- qwench/angularJS/indexModule.js
+++ qwench/angularJS/indexModule.js
@@ -1,4 +1,25 @@
-var app = angular.module('indexApp', []);
-app.controller('contentCtr', function($scope) {
-	$scope.includeUrl = "questionsList.html";
+var app = angular.module('indexApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider.when("/questions",
+    {
+      templateUrl: "questionsList.html",
+      controller: "QuestionsCtr",
+      controllerAs: "app"
+    }
+  );
+});
+
+app.controller('QuestionsCtr', function($scope,$http) {
+	$scope.questions =[];
+	
+	$http.get('http://localhost:8080/question/all').success(function(data){
+		$scope.questions = data;
+	});
+});
+
+//example to use params. add to when :paramName. like that .when("/questions/:param1"
+app.controller('AppCtrl', function($routeParams) {
+  var self = this;
+  self.message = $routeParams.message;
 });
