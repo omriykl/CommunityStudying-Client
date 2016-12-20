1be27eb95b99e5228069b89810eb36148f6d60c2
Guy Yom Tov <guyyomtov@gmail.com>
2016-12-20 13:00:41 +0200

    Add filter

--- public_html/controllers/questionsList.js
+++ public_html/controllers/questionsList.js
@@ -1,8 +1,96 @@
 
-app.controller('QuestionsCtr', function($scope,$http) {
+app.controller('QuestionsCtr',  ['$scope', '$http', function($scope, $http) {
 	$scope.questions =[];
 	
 	$http.get(SERVER_APP_BASE_URL+'question/all').success(function(data){
 		$scope.questions = data;
 	});
-});
+        
+            $scope.selectedFaculty = null;
+    $scope.faculties = [];
+
+    $scope.selectedCourse = null;
+    $scope.courses = [];
+
+    $http({
+        method: 'GET',
+        url: 'http://localhost:8080/faculty/getall',
+        data: {
+            applicationId: 3
+        }
+    }).success(function(result) {
+        $scope.faculties = result;
+    });
+
+    $scope.facultySelected = function(item) {
+        //$scope.item.size.code = $scope.selectedItem.code
+        var id = item.id;
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/course/getByFaculty/?facultyId='.concat(id),
+            data: {
+                applicationId: 3
+            }
+        }).success(function(result) {
+            $scope.courses = result;
+        });
+    }
+
+        $scope.onAddQuestionNumber = function() {
+            var data = $.param({
+                faculty: $scope.selectedFaculty.id,
+                course: $scope.selectedCourse.value,
+                year: $scope.year,
+                moed: $scope.selectedMoed.value,
+                qnum: $scope.qnumber
+            });
+            var config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+
+            $http.post('http://localhost:8080/question/checkexist', data, config)
+                .success(function(data, status, headers, config) {
+                    $scope.PostDataResponse = data;
+                })
+                .error(function(data, status, header, config) {
+                    //   $scope.ResponseDetails = "Data: " + data +
+                    //	<hr />status: " + status +
+                    //       "<hr />headers: " + header +
+                    //       "<hr />config: " + config;
+                });
+
+        };
+        
+        
+        $scope.submit = function() {
+            var data = $.param({
+                faculty: $scope.selectedFaculty.id,
+                course: $scope.selectedCourse.value,
+                year: $scope.year,
+                moed: $scope.selectedMoed.value,
+                qnum: $scope.qnumber,
+                tags: $scope.tags,
+                freeText: $scope.freeText
+            });
+            var config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+
+            $http.post('http://localhost:8080/question/filter', data, config)
+                .success(function(data, status, headers, config) {
+                    $scope.questions = data;
+                })
+                .error(function(data, status, header, config) {
+                    //   $scope.ResponseDetails = "Data: " + data +
+                    //	<hr />status: " + status +
+                    //       "<hr />headers: " + header +
+                    //       "<hr />config: " + config;
+                });
+        };
+        
+        
+}]);
