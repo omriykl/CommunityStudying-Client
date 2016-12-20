2a561870082ac0448d073c717592b83708429718
Guy Yom Tov <guyyomtov@gmail.com>
2016-12-20 10:57:59 +0200

    fix

--- public_html/controllers/newQuestion.js
+++ public_html/controllers/newQuestion.js
@@ -1,133 +1,137 @@
-
 app.controller('newQuestion', function($scope) {
     $scope.firstname = "John";
     $scope.htmlContent = 'Place your question';
-	
+
     $scope.selectedFaculty = null;
     $scope.faculties = [];
-	
+
     $scope.selectedCourse = null;
     $scope.courses = [];
-    
+
     $http({
-            method: 'GET',
-            url: 'http://localhost:8080/faculty/getall',
-            data: { applicationId: 3 }
-        }).success(function (result) {
+        method: 'GET',
+        url: 'http://localhost:8080/faculty/getall',
+        data: {
+            applicationId: 3
+        }
+    }).success(function(result) {
         $scope.faculties = result;
     });
-	
-	$scope.facultySelected = function(id) {
-   	//$scope.item.size.code = $scope.selectedItem.code
-	id = event.target.id;
-    $http({
+
+    $scope.facultySelected = function(id) {
+        //$scope.item.size.code = $scope.selectedItem.code
+        id = event.target.id;
+        $http({
             method: 'GET',
             url: 'http://localhost:8080/course/getByFaculty/?facultyId='.concat(id),
-            data: { applicationId: 3 }
-        }).success(function (result) {
-        $scope.courses = result;
-    });
-	
-	$scope.onAddQuestionNumber= function(){
-		var data=$.param({
-                    faculty: selectedFaculty,id,
-                    course: $scope.selectedCourse.value,
-                    year: $scope.year,
-                    moed: $scope.selectedMoed.value,
-                    qnum:  $scope.qnumber
-            });	
-		var config = {
-                headers : {
+            data: {
+                applicationId: 3
+            }
+        }).success(function(result) {
+            $scope.courses = result;
+        });
+    }
+
+        $scope.onAddQuestionNumber = function() {
+            var data = $.param({
+                faculty: selectedFaculty,
+                id,
+                course: $scope.selectedCourse.value,
+                year: $scope.year,
+                moed: $scope.selectedMoed.value,
+                qnum: $scope.qnumber
+            });
+            var config = {
+                headers: {
                     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
                 }
             }
-			
+
             $http.post('http://localhost:8080/question/checkexist', data, config)
-            .success(function (data, status, headers, config) {
-                $scope.PostDataResponse = data;
-            })
-            .error(function (data, status, header, config) {
-             //   $scope.ResponseDetails = "Data: " + data +
-			//	<hr />status: " + status +
-             //       "<hr />headers: " + header +
-             //       "<hr />config: " + config;
+                .success(function(data, status, headers, config) {
+                    $scope.PostDataResponse = data;
+                })
+                .error(function(data, status, header, config) {
+                    //   $scope.ResponseDetails = "Data: " + data +
+                    //	<hr />status: " + status +
+                    //       "<hr />headers: " + header +
+                    //       "<hr />config: " + config;
+                });
+
+        };
+
+        $scope.submit = function() {
+            var data = $.param({
+                faculty: selectedFaculty,
+                id,
+                course: $scope.selectedCourse.value,
+                year: $scope.year,
+                moed: $scope.selectedMoed.value,
+                qnum: $scope.qnumber,
+                title: $scope.title,
+                content: $scope.htmlContent
             });
-								
-	};
-        
-	$scope.submit = function() {
-        var data=$.param({
-                    faculty: selectedFaculty,id,
-                    course: $scope.selectedCourse.value,
-                    year: $scope.year,
-                    moed: $scope.selectedMoed.value,
-                    qnum:  $scope.qnumber,
-                    title: $scope.title,
-                    content: $scope.htmlContent
-            });	
-		var config = {
-                headers : {
+            var config = {
+                headers: {
                     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
                 }
             }
-			
+
             $http.post('http://localhost:8080/question/checkexist', data, config)
-            .success(function (data, status, headers, config) {
-                $scope.PostDataResponse = data;
-            })
-            .error(function (data, status, header, config) {
-             //   $scope.ResponseDetails = "Data: " + data +
-			//	<hr />status: " + status +
-             //       "<hr />headers: " + header +
-             //       "<hr />config: " + config;
-            });
-      };
-	
-	
-   }
+                .success(function(data, status, headers, config) {
+                    $scope.PostDataResponse = data;
+                })
+                .error(function(data, status, header, config) {
+                    //   $scope.ResponseDetails = "Data: " + data +
+                    //	<hr />status: " + status +
+                    //       "<hr />headers: " + header +
+                    //       "<hr />config: " + config;
+                });
+        };
+
 });
 
 
 
 var app2 = angular.module('fileUpload', ['ngFileUpload']);
 
-app2.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
-    $scope.$watch('files', function () {
+app2.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function($scope, Upload, $timeout) {
+    $scope.$watch('files', function() {
         $scope.upload($scope.files);
     });
-    $scope.$watch('file', function () {
+    $scope.$watch('file', function() {
         if ($scope.file != null) {
-            $scope.files = [$scope.file]; 
+            $scope.files = [$scope.file];
         }
     });
     $scope.log = '';
 
-    $scope.upload = function (files) {
+    $scope.upload = function(files) {
         if (files && files.length) {
             for (var i = 0; i < files.length; i++) {
-              var file = files[i];
-              if (!file.$error) {
-                Upload.upload({
-                    url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-                    data: {
-                      username: $scope.username,
-                      file: file  
-                    }
-                }).then(function (resp) {
-                    $timeout(function() {
-                        $scope.log = 'file: ' +
-                        resp.config.data.file.name +
-                        ', Response: ' + JSON.stringify(resp.data) +
-                        '\n' + $scope.log;
+                var file = files[i];
+                if (!file.$error) {
+                    Upload.upload({
+                        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                        data: {
+                            username: $scope.username,
+                            file: file
+                        }
+                    }).then(function(resp) {
+                        $timeout(function() {
+                            $scope.log = 'file: ' +
+                                resp.config.data.file.name +
+                                ', Response: ' + JSON.stringify(resp.data) +
+                                '\n' + $scope.log;
+                        });
+                    }, null, function(evt) {
+                        var progressPercentage = parseInt(100.0 *
+                            evt.loaded / evt.total);
+                        $scope.log = 'progress: ' + progressPercentage +
+                            '% ' + evt.config.data.file.name + '\n' +
+                            $scope.log;
                     });
-                }, null, function (evt) {
-                    var progressPercentage = parseInt(100.0 *
-                    		evt.loaded / evt.total);
-                    $scope.log = 'progress: ' + progressPercentage + 
-                    	'% ' + evt.config.data.file.name + '\n' + 
-                      $scope.log;
-                });
-              }
+                }
             }
         }
     };
@@ -137,6 +141,6 @@ app2.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upl
 
 //example to use params. add to when :paramName. like that .when("/questions/:param1"
 app.controller('AppCtrl', function($routeParams) {
-  var self = this;
-  self.message = $routeParams.message;
+    var self = this;
+    self.message = $routeParams.message;
 });
