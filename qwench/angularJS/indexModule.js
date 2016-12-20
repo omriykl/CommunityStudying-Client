afc2c737e9d36d0bd926f29ec84632efcd58b1de
ohirshman <hirshmanor@gmail.com>
2016-12-20 10:46:13 +0200

    google log in

--- qwench/angularJS/indexModule.js
+++ qwench/angularJS/indexModule.js
@@ -18,6 +18,34 @@ app.config(function($routeProvider){
 });
 
 
+app.controller('LoginCtr', function($scope,$http) {
+	$scope.isConnected = false;
+	$scope.login = function(googleUser){
+		console.log('Logged in as: ' + googleUser.getBasicProfile().getName());      
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+		$http.get('http://localhost:8080/user/getOrCreateUser?idTokenString='+id_token).success(function(user){
+		console.log(user);
+		$scope.userName=user.firstName;
+		$scope.isConnected = true;
+		
+    });
+}
+$scope.logout = function(){
+	$scope.isConnected = false;
+}
+});
+
+
 //example to use params. add to when :paramName. like that .when("/questions/:param1"
 app.controller('AppCtrl', function($routeParams) {
   var self = this;
