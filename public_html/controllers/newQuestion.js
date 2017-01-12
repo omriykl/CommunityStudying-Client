1530966c88d5e5431c4f001c2d07a1111b687683
Guy Yom Tov <guyyomtov@gmail.com>
2017-01-12 11:24:59 +0200

    have to add file

--- public_html/controllers/newQuestion.js
+++ public_html/controllers/newQuestion.js
@@ -1,40 +1,41 @@
+app.controller('newQuestion', ['$scope', '$http', 'Upload', '$timeout', function($scope, $http, Upload, $timeout) {
 
-app.controller('newQuestion', ['$scope', '$http', function($scope, $http) {
+    // Get the modal
+    var newTestModal = document.getElementById('newTestModal');
 
-            // Get the modal
-        var newTestModal = document.getElementById('newTestModal');
+    var btn = document.getElementById("myBtn");
 
-        var btn = document.getElementById("myBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementById("closeModel");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementById("closeModel");
-        
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-            newTestModal.style.display = "block";
-        };
-        $scope.openNewTestDialog=function(){
-            newTestModal.style.display = "block";
-        };
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        newTestModal.style.display = "block";
+    };
+    $scope.openNewTestDialog = function() {
+        newTestModal.style.display = "block";
+    };
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        newTestModal.style.display = "none";
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == newTestModal) {
             newTestModal.style.display = "none";
-        };
+        }
+    };
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == newTestModal) {
-                newTestModal.style.display = "none";
-            }
-        };
-        
-        var questionExistModel = document.getElementById('questionExistModel');
+    var questionExistModel = document.getElementById('questionExistModel');
 
-        var span2 = document.getElementById("closeQuestModel");
-        span2.onclick = function() {
-            //questionExistModel.style.display = "none";
-            $("#questionExistModel").fadeOut();
-        };
+    var span2 = document.getElementById("closeQuestModel");
+    span2.onclick = function() {
+        //questionExistModel.style.display = "none";
+        $("#questionExistModel").fadeOut();
+    };
+
+    $scope.mustAddFile = false; //on default
 
     $scope.loadFaculties = function() {
         $http({
@@ -48,7 +49,7 @@ app.controller('newQuestion', ['$scope', '$http', function($scope, $http) {
 
     $scope.$on('user-loaded', function(event, args) {
         $scope.loadFaculties(); // second call to get faculties, but this time after user is signed in! 
-        $scope.isConnected=true;
+        $scope.isConnected = true;
     });
 
     $scope.facultySelected = function(item) {
@@ -61,31 +62,31 @@ app.controller('newQuestion', ['$scope', '$http', function($scope, $http) {
             $scope.courses = result.allData;
         });
     };
-    
-     $scope.moedSelected = function(item) {
-        var params="facultyId="+$scope.faculty.id+
-                    "&courseId="+$scope.course.id+
-                    "&year="+$scope.year+
-                    "&semester="+ $scope.selectedSemester+
-                    "&moed="+$scope.selectedMoed;
+
+    $scope.moedSelected = function(item) {
+        var params = "facultyId=" + $scope.faculty.id +
+            "&courseId=" + $scope.course.id +
+            "&year=" + $scope.year +
+            "&semester=" + $scope.selectedSemester +
+            "&moed=" + $scope.selectedMoed;
         $http({
             method: 'GET',
             url: SERVER_APP_BASE_URL + 'post/checkByMoed?'.concat(params),
         }).success(function(result) {
-            if(result==false){
+            if (result == false) {
                 newTestModal.style.display = "block";
             }
         });
     };
-    
-    
-       $scope.searchTests = function() {
+
+
+    $scope.searchTests = function() {
         var data = {
             facultyId: $scope.faculty != null ? $scope.faculty.id : null,
             courseId: $scope.course != null ? $scope.course.id : null,
             year: $scope.year,
             semester: $scope.selectedSemester,
-            moed: $scope.selectedMoed, 
+            moed: $scope.selectedMoed,
 
         };
         var config = {
@@ -96,13 +97,12 @@ app.controller('newQuestion', ['$scope', '$http', function($scope, $http) {
 
         $http.post(SERVER_APP_BASE_URL + 'test/search', data, config)
             .success(function(data, status, headers, config) {
-                if(data==null || data.length==0){
+                if (data == null || data.length == 0) {
                     $("#testNotExistModel").fadeIn();
-                    $scope.mustAddFile=true; 
-                }
-                else{
-                     $("#testNotExistModel").fadeOut();
-                     $scope.mustAddFile=false; 
+                    $scope.mustAddFile = true;
+                } else {
+                    $("#testNotExistModel").fadeOut();
+                    $scope.mustAddFile = false;
                 }
             })
             .error(function(data, status, header, config) {
@@ -130,11 +130,10 @@ app.controller('newQuestion', ['$scope', '$http', function($scope, $http) {
 
         $http.post(SERVER_APP_BASE_URL + 'post/checkByQuestion', data, config)
             .success(function(data, status, headers, config) {
-                if(data==true){
-                   $("#questionExistModel").fadeIn();
-                   
-                }
-                else{
+                if (data == true) {
+                    $("#questionExistModel").fadeIn();
+
+                } else {
                     $("#questionExistModel").fadeOut();
                 }
             })
@@ -155,23 +154,23 @@ app.controller('newQuestion', ['$scope', '$http', function($scope, $http) {
             url: SERVER_APP_BASE_URL + 'tag/getAllByCourseId/?courseId='.concat(id),
         }).success(function(result) {
             $scope.optionsTags = result;
-             document.getElementById('tagsDiv').style.display="inline";
+            document.getElementById('tagsDiv').style.display = "inline";
         });
     }
-    
-     $scope.addTag = function() {
+
+    $scope.addTag = function() {
         var name = $scope.newTag;
         var id = $scope.course.id;
-        if(name!=null && name!=""){
+        if (name != null && name != "") {
             $http({
                 method: 'GET',
-                url: SERVER_APP_BASE_URL + 'tag/addTagToCourse/?courseId='+id+"&tagName="+name,
+                url: SERVER_APP_BASE_URL + 'tag/addTagToCourse/?courseId=' + id + "&tagName=" + name,
             }).success(function(result) {
                 $scope.optionsTags.push(result);
                 $scope.selectedTags.push(result);
-                $scope.newTag="";
+                $scope.newTag = "";
             });
-        }      
+        }
     };
 
     $scope.showName = function(item) {
@@ -181,52 +180,86 @@ app.controller('newQuestion', ['$scope', '$http', function($scope, $http) {
         return item.nameHebrew;
     }
 
+    $scope.filesIds = []; //empty file ids
+
+
+    $scope.addTest = function() {
+        if ($scope.filesIds.length == 0) {
+
+        } else {
+            $('#loading_image').show();
+            var data = {
+                facultyId: $scope.faculty.id,
+                courseId: $scope.course.id,
+                year: $scope.year,
+                semester: $scope.selectedSemester,
+                moed: $scope.selectedMoed,
+                files: $scope.filesIds
+            };
+            var config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            $http.post(SERVER_APP_BASE_URL + 'test/?userTokenId=' + USER_TOKEN, data, config)
+                .success(function(data, status, headers, config) {
+                    $scope.PostDataResponse = data;
+                    newTestModal.style.display = "none";
+                })
+                .error(function(data, status, header, config) {
+                    //   $scope.ResponseDetails = "Data: " + data +
+                    //	<hr />status: " + status +
+                    //       "<hr />headers: " + header +
+                    //       "<hr />config: " + config;
+                });
+        }
+
+    };
 
 
     $scope.submit = function() {
-        if($scope.mustAddFile){
-             $("#testNotExistModel").focus();
-             $("#testNotExistModel").fadeTo('slow', 0.5).fadeTo('slow', 1.0);
-        }
-        else{
-              $('#loading_image').show();
+        if ($scope.mustAddFile) {
+            $("#testNotExistModel").focus();
+            $("#testNotExistModel").fadeTo('slow', 0.5).fadeTo('slow', 1.0);
+        } else {
+            $('#loading_image').show();
             var data = {
-            facultyId: $scope.faculty.id,
-            courseId: $scope.course.id,
-            year: $scope.year,
-            semester: $scope.selectedSemester,
-            moed: $scope.selectedMoed,
-            questionNumber: $scope.qnumber,
-            title: $scope.title,
-            content: $scope.htmlContent,
-            tags: $scope.selectedTags
+                facultyId: $scope.faculty.id,
+                courseId: $scope.course.id,
+                year: $scope.year,
+                semester: $scope.selectedSemester,
+                moed: $scope.selectedMoed,
+                questionNumber: $scope.qnumber,
+                title: $scope.title,
+                content: $scope.htmlContent,
+                tags: $scope.selectedTags,
+                files: $scope.filesIds
                 //files: $scope.files
-        };
-        var config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
+            };
+            var config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
 
-        $http.post(SERVER_APP_BASE_URL + 'post/?userTokenId='+USER_TOKEN, data, config)
-            .success(function(data, status, headers, config) {
-                $scope.PostDataResponse = data;
-                window.location = "#questions/view/" + data.id;
-            })
-            .error(function(data, status, header, config) {
-                //   $scope.ResponseDetails = "Data: " + data +
-                //	<hr />status: " + status +
-                //       "<hr />headers: " + header +
-                //       "<hr />config: " + config;
-            });
+            $http.post(SERVER_APP_BASE_URL + 'post/?userTokenId=' + USER_TOKEN, data, config)
+                .success(function(data, status, headers, config) {
+                    $scope.PostDataResponse = data;
+                    window.location = "#questions/view/" + data.id;
+                })
+                .error(function(data, status, header, config) {
+                    //   $scope.ResponseDetails = "Data: " + data +
+                    //	<hr />status: " + status +
+                    //       "<hr />headers: " + header +
+                    //       "<hr />config: " + config;
+                });
         }
-      
+
     };
 
-}]);
 
 
-app.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function($scope, Upload, $timeout) {
     $scope.$watch('files', function() {
         $scope.upload($scope.files);
     });
@@ -238,18 +271,31 @@ app.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function($scope, Uploa
     $scope.log = '';
 
     $scope.upload = function(files) {
+        var bar = $('.progress');
+        var percent = $('.percent');
+        var submit = document.getElementById("submit");
+        var submit2 = document.getElementById("submitTest");
         if (files && files.length) {
             for (var i = 0; i < files.length; i++) {
                 var file = files[i];
                 if (!file.$error) {
+                    submit.disabled = true;
+                    submit2.disabled = true;
+                    var percentVal = '0%';
+                    bar.width(percentVal);
+                    percent.html(percentVal);
                     Upload.upload({
                         url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
                         data: {
-                            username: $scope.username,
+                            USER_TOKEN: USER_TOKEN,
                             file: file
                         }
                     }).then(function(resp) {
                         $timeout(function() {
+                            $scope.filesIds.push(resp.id);
+                            $scope.mustAddFile = false;
+                            submit.disabled = false;
+                            submit2.disabled = false;
                             $scope.log = 'file: ' +
                                 resp.config.data.file.name +
                                 ', Response: ' + JSON.stringify(resp.data) +
@@ -261,9 +307,58 @@ app.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function($scope, Uploa
                         $scope.log = 'progress: ' + progressPercentage +
                             '% ' + evt.config.data.file.name + '\n' +
                             $scope.log;
+
+                        var percentVal = progressPercentage + '%';
+                        bar.width(percentVal);
+                        percent.html(percentVal);
+
                     });
                 }
             }
         }
     };
+
 }]);
+
+//
+//app.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function($scope, Upload, $timeout) {
+//    $scope.$watch('files', function() {
+//        $scope.upload($scope.files);
+//    });
+//    $scope.$watch('file', function() {
+//        if ($scope.file != null) {
+//            $scope.files = [$scope.file];
+//        }
+//    });
+//    $scope.log = '';
+//
+//    $scope.upload = function(files) {
+//        if (files && files.length) {
+//            for (var i = 0; i < files.length; i++) {
+//                var file = files[i];
+//                if (!file.$error) {
+//                    Upload.upload({
+//                        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+//                        data: {
+//                            username: $scope.username,
+//                            file: file
+//                        }
+//                    }).then(function(resp) {
+//                        $timeout(function() {
+//                            $scope.log = 'file: ' +
+//                                resp.config.data.file.name +
+//                                ', Response: ' + JSON.stringify(resp.data) +
+//                                '\n' + $scope.log;
+//                        });
+//                    }, null, function(evt) {
+//                        var progressPercentage = parseInt(100.0 *
+//                            evt.loaded / evt.total);
+//                        $scope.log = 'progress: ' + progressPercentage +
+//                            '% ' + evt.config.data.file.name + '\n' +
+//                            $scope.log;
+//                    });
+//                }
+//            }
+//        }
+//    };
+//}]);
--- public_html/controllers/testsList.js
+++ public_html/controllers/testsList.js
@@ -3,41 +3,41 @@ var toggleSearch = function() {
         jQuery('#searchPanel').toggle('show');
     });
 };
-app.controller('TestsCtr', ['$scope', '$http','$routeParams', function($scope, $http, $routeParams) {
+app.controller('TestsCtr', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
     toggleSearch();
-     var newTestModal = document.getElementById('newTestModal');
+    var newTestModal = document.getElementById('newTestModal');
 
-        var btn = document.getElementById("myBtn");
+    var btn = document.getElementById("myBtn");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementById("closeModel");
-        
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-            newTestModal.style.display = "block";
-        };
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
+    // Get the <span> element that closes the modal
+    var span = document.getElementById("closeModel");
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        newTestModal.style.display = "block";
+    };
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        newTestModal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == newTestModal) {
             newTestModal.style.display = "none";
         }
+    };
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == newTestModal) {
-                newTestModal.style.display = "none";
-            }
-        };
-    
     $scope.tests = [];
-    
-    
-      $scope.searchTests = function() {
+
+
+    $scope.searchTests = function() {
         var data = {
             facultyId: $scope.faculty != null ? $scope.faculty.id : null,
             courseId: $scope.course != null ? $scope.course.id : null,
             year: $scope.year,
             semester: $scope.selectedSemester,
-            moed: $scope.selectedMoed, 
+            moed: $scope.selectedMoed,
 
         };
         var config = {
@@ -49,7 +49,7 @@ app.controller('TestsCtr', ['$scope', '$http','$routeParams', function($scope, $
         $http.post(SERVER_APP_BASE_URL + 'test/search', data, config)
             .success(function(data, status, headers, config) {
                 $scope.tests = data;
-                if(data.length==0){
+                if (data.length == 0) {
                     newTestModal.style.display = "block";
                 }
             })
@@ -60,39 +60,39 @@ app.controller('TestsCtr', ['$scope', '$http','$routeParams', function($scope, $
                 //       "<hr />config: " + config;
             });
     };
-    
-    if($routeParams.param!=null){
-        if($routeParams.param.includes("userId=")){
-            var id=$routeParams.param.split('userId=')[1];
-           $http({
+
+    if ($routeParams.param != null) {
+        if ($routeParams.param.includes("userId=")) {
+            var id = $routeParams.param.split('userId=')[1];
+            $http({
+                method: 'GET',
+                url: SERVER_APP_BASE_URL + 'test/getByUser?id=' + id,
+            }).success(function(result) {
+                $scope.tests = result;
+            });
+        }
+    } else {
+        $http({
             method: 'GET',
-            url: SERVER_APP_BASE_URL + 'test/getByUser?id='+id,
-        }).success(function(result) {
-            $scope.tests = result;
-        });}
-    }
-    else{
-    $http({
-            method: 'GET',
-            url: SERVER_APP_BASE_URL + 'test/getByUser?id='+USER_TOKEN
+            url: SERVER_APP_BASE_URL + 'test/getByUser?id=' + USER_TOKEN
         }).success(function(result) {
             $scope.tests = result;
         });
     }
-//$scope.tests=[{
-//        userName : "guyyt",
-//        comments : "3",
-//        createdAt: "1.1.2016",
-//        posts : "5",
-//           title : "my quest",
-//           content : "hdfhsdkhfk shkdfjhsdkfj hsdkj",
-//           timeAgo: "1.1.2016",
-//           year: "2016",
-//           semester: "A",
-//           moed: "B",
-//            hasTestFile: true
-//           
-//}];
+    //$scope.tests=[{
+    //        userName : "guyyt",
+    //        comments : "3",
+    //        createdAt: "1.1.2016",
+    //        posts : "5",
+    //           title : "my quest",
+    //           content : "hdfhsdkhfk shkdfjhsdkfj hsdkj",
+    //           timeAgo: "1.1.2016",
+    //           year: "2016",
+    //           semester: "A",
+    //           moed: "B",
+    //            hasTestFile: true
+    //           
+    //}];
 
     $scope.selectedFaculty = null;
     $scope.faculties = [];
@@ -131,40 +131,95 @@ app.controller('TestsCtr', ['$scope', '$http','$routeParams', function($scope, $
     $scope.showHebName = function(item) {
         return item.nameHebrew;
     };
-    
+
     $scope.submit = function() {
         $scope.searchTests();
     };
-    
-     $scope.addTest = function() {
-        $('#loading_image').show();
-        var data = {
-            facultyId: $scope.faculty.id,
-            courseId: $scope.course.id,
-            year: $scope.year,
-            semester: $scope.selectedSemester,
-            moed: $scope.selectedMoed 
-        };
-        var config = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        };
 
-        $http.post(SERVER_APP_BASE_URL + 'test/?userTokenId='+USER_TOKEN, data, config)
-            .success(function(data, status, headers, config) {
-                $scope.PostDataResponse = data;
-                newTestModal.style.display = "none";
-            })
-            .error(function(data, status, header, config) {
-                //   $scope.ResponseDetails = "Data: " + data +
-                //	<hr />status: " + status +
-                //       "<hr />headers: " + header +
-                //       "<hr />config: " + config;
-            });
+    $scope.filesIds = []; //empty file ids
+
+    $scope.addTest = function() {
+        if ($scope.filesIds.length == 0) {
+
+        } else {
+            $('#loading_image').show();
+            var data = {
+                facultyId: $scope.faculty.id,
+                courseId: $scope.course.id,
+                year: $scope.year,
+                semester: $scope.selectedSemester,
+                moed: $scope.selectedMoed,
+                files: $scope.filesIds
+            };
+            var config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            $http.post(SERVER_APP_BASE_URL + 'test/?userTokenId=' + USER_TOKEN, data, config)
+                .success(function(data, status, headers, config) {
+                    $scope.PostDataResponse = data;
+                    newTestModal.style.display = "none";
+                })
+                .error(function(data, status, header, config) {
+                    //   $scope.ResponseDetails = "Data: " + data +
+                    //	<hr />status: " + status +
+                    //       "<hr />headers: " + header +
+                    //       "<hr />config: " + config;
+                });
+        }
+
     };
-    
-    
+
+
+
+    $scope.upload = function(files) {
+        var bar = $('.progress');
+        var percent = $('.percent');
+        var submit2 = document.getElementById("submitTest");
+        if (files && files.length) {
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                if (!file.$error) {
+                    submit2.disabled = true;
+                    var percentVal = '0%';
+                    bar.width(percentVal);
+                    percent.html(percentVal);
+                    Upload.upload({
+                        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                        data: {
+                            USER_TOKEN: USER_TOKEN,
+                            file: file
+                        }
+                    }).then(function(resp) {
+                        $timeout(function() {
+                            $scope.filesIds.push(resp.id);
+                            $scope.mustAddFile = false;
+                            submit2.disabled = false;
+                            $scope.log = 'file: ' +
+                                resp.config.data.file.name +
+                                ', Response: ' + JSON.stringify(resp.data) +
+                                '\n' + $scope.log;
+                        });
+                    }, null, function(evt) {
+                        var progressPercentage = parseInt(100.0 *
+                            evt.loaded / evt.total);
+                        $scope.log = 'progress: ' + progressPercentage +
+                            '% ' + evt.config.data.file.name + '\n' +
+                            $scope.log;
+
+                        var percentVal = progressPercentage + '%';
+                        bar.width(percentVal);
+                        percent.html(percentVal);
+
+                    });
+                }
+            }
+        }
+    };
+
+
 
 
 }]);
